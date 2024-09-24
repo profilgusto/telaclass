154a61e4ee1dfd1b4c8a8d9027e5ba920734a3ec
Filipe Augusto Santos Rocha <filipe@ufsj.edu.br>
2024-09-24 17:11:00 -0300

    Created export-to-production file

---
+++ production-to-rpis110.sh
@@ -0,0 +1,5 @@
+# react app
+rsync -avz /Users/filgusto/git-sources/telaclass filgusto@rpis110:/home/filgusto/telaclass-main
+
+# course content
+rsync -avz /Users/filgusto/pcloud-sync/ufsj/disciplinas/ENM704_sistemas-supervisorios/material/obsidian/content_supervisorios filgusto@rpis110:/home/filgusto/telaclass-main/content-data
