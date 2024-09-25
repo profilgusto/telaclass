69eb78f8be06082074846349c2dd5e7ec2366403
Filipe Augusto Santos Rocha <filipe@ufsj.edu.br>
2024-09-25 19:41:30 -0300

    App is rendering in Content component the content from a .mdx file in runtime successfullyyyy

--- react-app/src/components/Content/index.js
+++ react-app/src/components/Content/index.js
@@ -20,9 +20,11 @@ const Content = ({ planoDeAulas, selectedLesson, content_url }) => {
   // Defining states
   const [mdxContent, setMdxContent] = useState(null);
 
+  // Fetching the MDX file
   useEffect(() => {
     const fetchMdx = async () => {
       try {
+        // TODO - Usar o seletor de aula para dar fetch no arquivo correto de aula
         const response = await fetch('http://localhost:3001/01_introducao.mdx');
         const data = await response.text();
         setMdxContent(data);
@@ -33,50 +35,7 @@ const Content = ({ planoDeAulas, selectedLesson, content_url }) => {
     fetchMdx();
   }, []);
 
-  //console.log(mdxContent);
-
-
-  
-  // Defining functions
-  /*
-  const treatMarkdownContent = (content) => {
-    const baseUrl = content_url+ATTACHMENTS_PATH;
-
-    // Treating PDF files
-    // TODO - Tratar também quando .PDF está em maiúsculo
-    content = content.replace(/!\[\[(.*?)\.pdf\]\]/g, `<embed src="${baseUrl}$1.pdf" width="600" height="500" type="application/pdf">`);
-    
-    // Treating images
-    // TODO - Pesquisar especificamente por arquivos com final .jpg ou .png ou .jpeg, assim como é feito com .pdf
-    content = content.replace(/!\[\[(.*?)\]\]/g, `![](${baseUrl}$1)`);
-
-    return content
-  }; */
-
-  // Fetching the content markdown file
-  /*
-  useEffect(() => {
-    if (selectedLesson!=null){ // if there is a selected lesson from the sidebar
-      const lessonPathMD = content_url + planoDeAulas[selectedLesson].path;
-      fetch(lessonPathMD)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
-      })
-      .then((text) => setMarkdownContent(treatMarkdownContent(text)))
-      .catch((error) => {
-        console.error('We could not load the markdown file:', error);
-      });
-    } else {
-      setMarkdownContent(null);
-    }
-  }, [selectedLesson]); 
-
-  */
-
-  // TODO - Consertar o content que tá ficando atrás de header quando o tamanho da tela diminui lateralmente.
+  // TODO - Tratar no mdx importado as figuras e etc
 
   // Caso ainda não haja nada selecionado...
   if (selectedLesson==null) {
@@ -86,37 +45,11 @@ const Content = ({ planoDeAulas, selectedLesson, content_url }) => {
   return (
     <main className="content">
 
-      <h1 
-        className="classTitle"
-      >
+      <h1 className="classTitle">
         {planoDeAulas[selectedLesson].titulo}
       </h1>
 
-      {/* MarkdownExtended content */}
-      <MDXProvider>
-        <MyMDXContent />
-      </MDXProvider>
-      
-
-      <h1>======OOOOO</h1>
-
-      <MDXRenderer mdxContent={mdxContent} />
-
-      {/* 
-
-      <MDXProvider>
-      <div>
-        {mdxContent ? (
-          <div>{mdx(mdxContent)}</div>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </MDXProvider>
-
-    */}
-
-      
+      <MDXRenderer mdxContent={mdxContent} />    
 
     </main>
   );
