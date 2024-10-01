a8e196910503f1b0f333bd30456d19b2d16d9b39
Filipe Augusto Santos Rocha <filipe@ufsj.edu.br>
2024-10-01 15:51:59 -0300

    Now math environment is styyylishhh

--- react-app/src/components/MdxRenderer/index.js
+++ react-app/src/components/MdxRenderer/index.js
@@ -8,8 +8,13 @@ import { MDXProvider } from '@mdx-js/react';
 import { compile, run } from '@mdx-js/mdx';
 import * as runtime from 'react/jsx-runtime'
 
+// plugins
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github-dark.css';
+import remarkGfm from 'remark-gfm' // Tables, footnotes, strikethrough, task lists, literal URLs.
+import remarkMath from 'remark-math' // Support math like `$so$`.
+import rehypeKatex from 'rehype-katex' // Render math with KaTeX.
+
 
 const MDXRenderer = ({ mdxContent }) => {
   const [content, setContent] = useState(null);
@@ -17,7 +22,11 @@ const MDXRenderer = ({ mdxContent }) => {
 
   useEffect(() => {
     const renderMDX = async () => {
-      const compiled = await compile(await mdxContent, { outputFormat: 'function-body' });
+      const compiled = await compile(await mdxContent, { 
+          outputFormat: 'function-body',
+          rehypePlugins: [rehypeKatex], 
+          remarkPlugins: [remarkMath, remarkGfm],
+       });
       const {default: ContentMdx} = await run(compiled, {...runtime, baseUrl: import.meta.url});
       setContent(<ContentMdx />);
     };
@@ -39,6 +48,7 @@ const MDXRenderer = ({ mdxContent }) => {
   
   return (
     <div ref={mdxContentRef}>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.css" integrity="sha384-NFTC4wvyQKLwuJ8Ez9AvPNBv8zcC2XaQzXSMvtORKw28BdJbB2QE8Ka+OyrIHcQJ" crossorigin="anonymous"></link>
       <MDXProvider>{content}</MDXProvider>
     </div>
   );
