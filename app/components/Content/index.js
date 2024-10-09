ca4d43b81a50604a1c4d4b286a82f37465b6989a
Filipe Augusto Santos Rocha <filipe@ufsj.edu.br>
2024-10-09 09:49:11 -0300

    Treated the fetching of text and slides contents

--- app/components/Content/index.js
+++ app/components/Content/index.js
@@ -5,31 +5,58 @@ import MDXRenderer from '../MdxRenderer';
 // Import styles
 import styles from './style.module.css';
 
+// CONSTANTS
+const FETCH_SUCCESS = 200;
+const FETCH_NOT_FOUND = 404;
+
 
 const Content = ({ modulos, selectedLesson, content_url: contentUrl }) => {
 
-
   // Defining states
   const [mdxContentText, setMdxContentText] = useState(null);
+  const [mdxContentSlides, setMdxContentSlides] = useState(null);
 
   // Fetching the text MDX file
   useEffect(() => {
-    const fetchContentTextMdx = async () => {
+    const fetchMdxContent = async () => {
       if (selectedLesson != null && modulos[selectedLesson]) {
+
+        // mounting the path to the class resources
+        const path_to_modulo = contentUrl + modulos[selectedLesson].path ;
+
+
+        // TODO tratar o fetch, pois não quero que ele gere uma mensagem de erro no console quando o arquivo não for encontrado
+        // Tratando do arquivo MDX do texto
+        let responseTexto;
         try {
-          const path_to_modulo = contentUrl + modulos[selectedLesson].path ;
-          const response = await fetch(path_to_modulo + '/texto.mdx');
-          const data = fixMdx(await response.text(), path_to_modulo);
-          setMdxContentText(data);
+          responseTexto = await fetch(path_to_modulo + '/texto.mdx');
+          if (responseTexto.status == FETCH_SUCCESS) {
+            const dataTexto = fixMdx(await responseTexto.text(), path_to_modulo);
+            setMdxContentText(dataTexto);
+          }
         } catch (error) {
-          console.error('Error fetching the MDX file:', error);
-          setMdxContentText('Erro ao carregar o arquivo .mdx [2]');
-        }  
-      } else {
-        setMdxContentText("Problema ao carregar o arquivo .mdx [1]");
+          // TODO Este erro nunca está sendo chamado, pelo menos não quando o arquivo não é encontrado (404)
+          console.error(`Erro ao fetchar o arquivo .mdx de texto. Error message: ${error}`);
+          throw error; // Re-throw the error to be caught by the outer catch
+        }
+
+        // Tratando do arquivo MDX dos slides
+        let responseSlides;
+        try {
+          responseSlides = await fetch(path_to_modulo + '/slide.mdx');
+          if (responseSlides.status == FETCH_SUCCESS) {
+            const dataSlides = fixMdx(await responseSlides.text(), path_to_modulo);
+            setMdxContentSlides(dataSlides);
+          }
+
+        } catch (error) {
+          // TODO Este erro nunca está sendo chamado, pelo menos não quando o arquivo não é encontrado (404)
+          console.error(`Erro ao fetchar o arquivo .mdx dos slides. Error message: ${error}`);
+          throw error; // Re-throw the error to be caught by the outer catch
+        }
       }
     };
-    fetchContentTextMdx();
+    fetchMdxContent();
   }, [selectedLesson]);
 
 
@@ -53,9 +80,24 @@ const Content = ({ modulos, selectedLesson, content_url: contentUrl }) => {
         <h1>{modulos[selectedLesson].titulo}</h1>
       </div>
 
-      <div className={styles.classContent}>
-        <MDXRenderer mdxContent={mdxContentText} />
-      </div>    
+      {mdxContentSlides && (
+        <div className={styles.classSlides}>
+          <MDXRenderer mdxContent={mdxContentSlides} />
+        </div>
+      )}
+
+      {mdxContentText && (
+        <div className={styles.classContent}>
+          <MDXRenderer mdxContent={mdxContentText} />
+        </div>    
+      )}
+
+      { !mdxContentSlides && !mdxContentText && (
+        <div className={styles.classContent}>
+          <p>Ainda não há conteúdo cadastrado para este módulo.</p>
+        </div>
+      )}
+
 
     </div>
   );
