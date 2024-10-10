2d20a3b6966003ac5fa69ecdef3bd7fd14c4edf1
Filipe Augusto Santos Rocha <filipe@ufsj.edu.br>
2024-10-10 09:01:14 -0300

    layouting slides now work!

--- app/components/SlidesRenderer/slidesToReactFlowNodes.js
+++ app/components/SlidesRenderer/slidesToReactFlowNodes.js
@@ -5,11 +5,34 @@ const slidesToReactFlowNodes = (slidesContent) => {
     const nodes = [];
     const edges = [];
 
-    let pos_x_current = 0;
-    let pos_y_current = 0;
+
+
+    let flag_first_slide = true;
+
+    let pos_x_current;
+    let pos_y_current;
 
 
     slidesContent.forEach( (slideContent, index) => {
+
+        // layouting slides
+        if (flag_first_slide) {
+            pos_x_current = 0;
+            pos_y_current = 0;
+            flag_first_slide = false;
+        } else {
+            if (slideContent.startsWith("##")) {
+                pos_y_current += SLIDE_HEIGHT * 1.05;
+            } else if (slideContent.startsWith("#")) {
+                pos_x_current += SLIDE_WIDTH * 1.2;
+                pos_y_current = 0;
+            } else {
+                const errorMessage = `There was a problem with the slide layouting. Maybe the .mdx file is being splitted uncorretly? Check it out. Each slide content should beging with '#' or '##'.`;
+                console.error(errorMessage);
+                throw new Error(errorMessage);
+            }
+        }
+
         nodes.push({
             id: index.toString(),
             data: {content: slideContent},
@@ -22,8 +45,6 @@ const slidesToReactFlowNodes = (slidesContent) => {
             source: index,
             target: index + 1
         });
-
-        pos_x_current += SLIDE_WIDTH * 1.05;
 
     });
 
